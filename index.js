c52e9ad1f08609a04495c737046a9f39a3360783
Ed Salisbury <ed.salisbury@gmail.com>
2021-09-24 00:55:13 -0600

    Added some error checking

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Autotweet for new youtube videos
+// Ed Salisbury
+// Last Modified: 09/24/2021
+
+const datefns = require('date-fns');
 const config = require('./config');
 const apiClient = config.newClient();
 const uploadClient = config.newClient('upload');
@@ -5,48 +10,91 @@ const axios = require('axios').default;
 
 const Parser = require('rss-parser');
 const parser = new Parser();
+const channelId = "UC9T7-AwG8ECs7AzmVHovQ8A"
 
-function getBase64(url) {
-  return axios
-    .get(url, {
-      responseType: 'arraybuffer'
-    })
-    .then(response => Buffer.from(response.data, 'binary').toString('base64'))
+const getBase64 = async(url) => {
+  try {
+    const response = await axios.get(url, {responseType: 'arraybuffer'});
+    return Buffer.from(response.data, 'binary').toString('base64');
+  } catch(e) {
+    error.log(e)
+  }
 }
 
 const getVideos = async() => {
-  const feed = await parser.parseURL("https://www.youtube.com/feeds/videos.xml?channel_id=UC9T7-AwG8ECs7AzmVHovQ8A")
-  const item = feed.items[0];
-  // feed.items.forEach(async item => {
+  try {
+    const feed = await parser.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`)
+  } catch(e) {
+    console.log("Error getting video feed")
+    return
+  }
+  const oneHourAgo = datefns.subHours(new Date(), 1);
+  for (const item of feed.items) {
+    const videoTime = new Date(item.pubDate);
+    if (videoTime < oneHourAgo) {
+      continue;
+    }
+
     const videoTitle = item.title;
     const videoId = item.id.split(":")[2]
     const videoLink = `https://youtu.be/${videoId}`
     const imgUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-    console.log(imgUrl);
-    const image = await getBase64(imgUrl);
-    if (image !== "") {
-      console.log("Tweeting");
-      const msg = `New video up! ${videoTitle} - ${videoLink}`;
-      tweet(msg, image);
+    console.log(`New video found: ${videoTitle}`);
+    try {
+      const image = await getBase64(imgUrl);
+    } catch(e) {
+      error.log(e)
     }
-    process.exit();
-  // })
+    if (image !== "") {
+      const msg = `New video up! ${videoTitle} - ${videoLink}`;
+      console.log(`Tweeting: ${msg}`);
+      try {
+        await tweet(msg, image);
+      } catch(e) {
+        error.log(e)
+      }
+    }
+  };
+
 }
 
-const tweet = (text, image) => {
+const tweet = async(text, image) => {
   let media_id = "";
-  uploadClient.post('media/upload', { media_data: image })
-  .then(media => {
-    console.log(media);
-    media_id = media.media_id_string;
-  }).catch(console.error);
 
+  console.log("Uploading image");
+  try {
+    const response = await uploadClient.post('media/upload', { media_data: image });
+    media_id = response.media_id_string;
+  } catch(e) {
+    error.log(e)
+  }
+  
   console.log(`media_id = ${media_id}`)
   if (media_id !== "") {
-    apiClient.post('statuses/update', { status: text, media_ids: media_id }).then(result => {
-      console.log(`Tweeted "${text}"`)
-    }).catch(console.error);
+    try {
+    const response = await apiClient.post('statuses/update', { status: text, media_ids: media_id });
+    console.log(`Tweeted "${text}"`)
+    } catch(e) {
+      error.log(e)
+    }
   }
 }
 
-getVideos();
+const main = async() => {
+  while (true) {
+    try {
+      await getVideos();
+    } catch(e) {
+      error.log(e)
+    }
+    const sleepTime = 60 * 60;
+    console.log(`Sleeping ${sleepTime} seconds...`);
+    try {
+      await new Promise(resolve => setTimeout(resolve, sleepTime * 1000));
+    } catch(e) {
+      error.log(e)
+    }
+  }
+}
+
+main();
